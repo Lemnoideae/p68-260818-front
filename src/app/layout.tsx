c53394e86fc9a5939cc1b845d048bf5936f062bf
Lemnoideae <aquan5584j@gmail.com>
2026-08-18 11:42:38 +0900

    6

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,26 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="ko"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body
+        className="
+        min-h-full
+        flex
+        flex-col"
+      >
+
+      <header>
+        <nav className="flex gap-4">
+          <a href="/">메인</a>
+          <a href="/posts">글 목록</a>
+          
+        </nav>
+      </header>
+      {children}
+      <footer>
+        푸터
+      </footer>
+
+      </body>
     </html>
   );
 }
